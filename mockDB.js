ba85c74f795d9933df000a0db66fe83db0fb0406
Hakjae415 <hakjae.k415@gmail.com>
2023-07-27 15:29:49 -0400

    created Pizza DB & map it out in App

---
+++ mockDB.js
@@ -0,0 +1,33 @@
+const pizzas = [
+    {
+      id:1,
+      name: "Pepperoni",
+      toppings: [
+        "cheese",
+        "pepperoni"
+      ]
+    },
+    {
+      id:2,
+      name: "Hawaiian",
+      toppings: [
+        "cheese",
+        "pineapple",
+        "ham"
+      ]
+    },
+    {
+      id:3,
+      name: "Supreme",
+      toppings: [
+        "cheese",
+        "onions",
+        "peppers",
+        "olives",
+        "mushrooms",
+        "pepperoni"
+      ]
+    },
+]
+
+export default pizzas
--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,27 @@
-import { useState } from 'react'
 import Header from './Header'
+import pizzas from '../mockDB'
 
 function App() {
-
+  console.log(pizzas)
   return (
     <>
       <Header/>
       <h1>Pizza Dave</h1>
+      {
+        pizzas.map((pizza) => {
+          return (
+            <>
+              <h3 key={pizza.id}>{pizza.name}</h3>
+              <p>Made By Jon</p>
+              {
+                pizza.toppings.map((topping) => {
+                  return <li key={topping}>{topping}</li>
+                })
+              }
+            </>
+          )
+        })
+      }
     </>
   )
 }
